5c05f3da7ab2c2cf74b026d473a4eee7356e6585
michealkusi42-bit <michealkusi42@gmail.com>
2026-06-17 00:38:24 +0300

    Refactor GameCard to use useNavigate instead of Link

--- src/components/game-card/index.tsx
+++ src/components/game-card/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface GameCardProps {
     name: string;
@@ -9,104 +9,100 @@ interface GameCardProps {
 }
 
 const GameCard = ({ name, image, href }: GameCardProps) => {
+    const navigate = useNavigate();
+
     return (
-        <Link to={`${href}`}>
+        <Box
+            onClick={() => navigate(href)}
+            sx={{
+                position: 'relative',
+                borderRadius: 2,
+                overflow: 'hidden',
+                cursor: 'pointer',
+                '&:hover .overlay': { opacity: 1 },
+                '&:hover .play-button': {
+                    transition: 'transform 0.3s',
+                    transform: 'scale(1.5)'
+                },
+                pt: '124%',
+                '&::before': {
+                    content: `""`,
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    filter: 'blur(10px)'
+                }
+            }}
+        >
             <Box
                 sx={{
-                    position: 'relative',
-                    borderRadius: 2,
-                    overflow: 'hidden',
-                    '&:hover .overlay': {
-                        opacity: 1
-                    },
-                    '&:hover .play-button': {
-                        transition: 'transform 0.3s',
-                        transform: 'scale(1.5)'
-                    },
-                    pt: '124%',
+                    height: 1,
+                    width: 1,
+                    zIndex: 1,
+                    top: 0,
+                    right: 0,
+                    position: 'absolute',
+                    backgroundImage: `url(${image})`,
                     backgroundRepeat: 'no-repeat',
                     backgroundPosition: 'center center',
-                    backgroundSize: '150%',
-                    '&::before': {
-                        content: `""`,
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        backgroundImage: `url(${image})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        filter: 'blur(10px)'
-                    }
+                    backgroundSize: { xs: 'cover', sm: '100% 100%' },
+                    backgroundColor: 'background.default'
+                }}
+            />
+            <Box
+                className="overlay"
+                sx={{
+                    zIndex: 2,
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    width: '100%',
+                    height: '100%',
+                    bgcolor: 'rgba(0, 0, 0, 0.6)',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    opacity: 0,
+                    transition: 'opacity 0.3s',
                 }}
             >
-                <Box
+                <Typography
                     sx={{
-                        height: 1,
-                        width: 1,
-                        zIndex: 1,
-                        top: 0,
-                        right: 0,
                         position: 'absolute',
-                        backgroundImage: `url(${image})`,
-                        backgroundRepeat: 'no-repeat',
-                        backgroundPosition: 'center center',
-                        backgroundSize: { xs: 'cover', sm: '100% 100%' },
-                        backgroundColor: 'background.default'
-                    }}
-                />
-                <Box
-                    className="overlay"
-                    sx={{
-                        zIndex: 2,
-                        position: 'absolute',
-                        left: 0,
-                        top: 0,
+                        top: '20%',
                         width: '100%',
-                        height: '100%',
-                        bgcolor: 'rgba(0, 0, 0, 0.6)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        opacity: 0,
-                        transition: 'opacity 0.3s',
-                        cursor: 'pointer'
+                        px: 2,
+                        textAlign: 'center',
+                        fontWeight: 800,
+                        fontSize: '0.875rem',
+                        lineHeight: 1,
+                        color: 'white'
                     }}
                 >
-                    <Typography
-                        sx={{
-                            position: 'absolute',
-                            top: '20%',
-                            width: '100%',
-                            px: 2,
-                            textAlign: 'center',
-                            fontWeight: 800,
-                            fontSize: '0.875rem',
-                            lineHeight: 1,
-                            color: 'white'
-                        }}
-                    >
-                        {name}
-                    </Typography>
-                    <Box
-                        className="play-button"
-                        sx={{
-                            width: 36,
-                            height: 36,
-                            borderRadius: '50%',
-                            bgcolor: 'rgba(255, 255, 255, 0.2)',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}
-                    >
-                        <PlayArrowIcon sx={{ color: 'white' }} />
-                    </Box>
+                    {name}
+                </Typography>
+                <Box
+                    className="play-button"
+                    sx={{
+                        width: 36,
+                        height: 36,
+                        borderRadius: '50%',
+                        bgcolor: 'rgba(255, 255, 255, 0.2)',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <PlayArrowIcon sx={{ color: 'white' }} />
                 </Box>
             </Box>
-        </Link>
+        </Box>
     );
 };
 
