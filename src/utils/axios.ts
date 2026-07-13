b7d64bc47b41d5a4d0e8ab9362a2efa643993a1b
michealkusi42-bit <michealkusi42@gmail.com>
2026-07-14 01:06:22 +0300

    Add request interceptor for x-admin-password header
    Added an interceptor to automatically attach the x-admin-password header to requests if it is stored in session storage.

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,6 +6,15 @@ import { HOST_API_KEY } from 'config';
 
 const axiosInstance = axios.create({ baseURL: HOST_API_KEY });
 
+// Automatically attach x-admin-password to every request if stored in session
+axiosInstance.interceptors.request.use((config) => {
+    const adminPassword = sessionStorage.getItem('adminPanelPassword');
+    if (adminPassword) {
+        config.headers['x-admin-password'] = adminPassword;
+    }
+    return config;
+});
+
 axiosInstance.interceptors.response.use(
     (response) => response,
     (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
